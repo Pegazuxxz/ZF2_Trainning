69a604af0888b31e357e5c1b80a21b605559bf2b
root <root@eagle2.eagledigital>
2015-07-10 15:36:26 -0300

    Commit Service_Manager

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -5,7 +5,7 @@ return array(
 		'driver' => 'Pdo',
 		'dsn'	 => 'mysql:dbname=zendFramework;host=localhost',
 		'driver_options' => array(
-			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF-8\''
+			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
 		
 		),
 	),
